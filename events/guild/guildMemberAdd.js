983b123a3b60bf4439221295e9c37489623a9c2b
KousenR <lastdescent@live.com>
2021-03-02 02:44:51 -0800

    tiny oopsies

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,4 +1,4 @@
-module.exports = (client, message, Discord) => {
+module.exports = (Discord, client, message) => {
     client.on('guildMemberAdd', guildMember =>{
             guildMember.guild.channels.cache.get('530010536864251917').send(`I guess I'm glad you're here, <@${guildMember.user.id}> ` 
                     + '...make sure to read the rules channel or else.')})
